82d54fa7d9b65f57ee9b24e7f04f2f42afacf1f5
Andre Mitchell <andre.mitchell@bytesofpi.com>
2023-12-21 20:55:34 -0500

    Change condition function template file name

---
+++ Assets/ConversationCanvas/GraphData/ConversationOutputs/Conditions/ConditionFunction.lua
@@ -0,0 +1,12 @@
+-- Generated condition function
+function ConversationGraphName:ConversationGraphNodeName()
+	local result = false
+
+	-- BOP_GENERATED_INSTRUCTIONS_BEGIN: outCondition
+
+	-- result needs to be assigned to a boolean expression.
+
+	-- BOP_GENERATED_INSTRUCTIONS_END
+
+	return result
+end
